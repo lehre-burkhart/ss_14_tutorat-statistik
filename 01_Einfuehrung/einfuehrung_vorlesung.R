aa7229ec951ce32439746b5710cf982d0b0114b8
Christian Burkhart <burhart.christian@gmail.com>
2014-05-02 14:51:06 +0200

    Datei hinzugefügt

---
+++ 01_Einfuehrung/einfuehrung_vorlesung.R
@@ -0,0 +1,33 @@
+##################################################################
+#
+# R | Einführung
+#
+# Albert-Ludwigs-Universität Freiburg
+# Institut für Erziehungswissenschaft
+# SS 2014
+# Studienfach: Bildungswissenschaft und Bildungsmanagement (B.A.)
+# Seminar: Statistik I
+# 02.05.2014
+#
+###################################################################
+
+
+# ************************** R | Skript ******************************
+# https://github.com/ch-bu/tutorat_statistik
+
+
+# Skript herunterladen
+# WD
+# Oberfläche
+# Variablen finden
+# Dataframe bedienen
+# help funktion
+# Packete installieren
+# Daten einlesen
+# grundlegende Funktionen
+  # names(), str(), describe(), nrow(), ncol(), 
+# weitere Funktionen
+  # subset(), tapply(), table(), attach(), 
+# deskriptive funktionen
+  # mean(), median(), max(), min(), var(), sd(), range(), which.max(), which.min(), quantile()
+  # hist(), boxplot()
