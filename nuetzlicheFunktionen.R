752b94712c242722725c583bfe5dd82d0e2efb69
Christian Burkhart <burkhart.christian@gmail.com>
2014-05-07 19:43:19 +0200

    Funktionen hinzugefügt

--- nuetzlicheFunktionen.R
+++ nuetzlicheFunktionen.R
@@ -16,6 +16,34 @@ sessionInfo() # Zeigt R-Version, Platform, lokale Variablen und welche Packete g
 chooseCRANmirror() # Wählt den Server aus, von dem die Packete installiert werden
 # install.packages(); z.B. install.packages("psych"); Installiert ein Packet aus dem CRANmirror
 # ?packet; z.B. ?psych; Zeigt Hilfe über das Packet
+library() # zeigt alle Packete an, die installiert sind
+library(help = stats) # Information, welche Funktionen ein Paket enth�lt
+packageDescription("stats") # �berpr�fen, wo Pakete installiert sind
+
+# Working Directory
+getwd() # Gibt Pfad des Working Directory zur�ck
+# setwd(Pfad) # Bestimmt das Working Directory
+
+# Daten einlesen
+# variablenName <- read.table("Datei.txt", header = TRUE)
+# variablenName <- read.csv("Datei.csv", header = TRUE, sep = ";") 
+
+# Dataframes
+?str # Gibt die Variablentypen im Dataframe zurück
+?names # Gibt die Namen der Variablen zurück
+?subset # Verkleinerung eines Dataframes mit verschiedenen Bedingungen
+?nrow # Anzahl der Reihen eines Dataframes
+?ncol # Anazl der Spalten eines Dataframes
+?head # Gibt die ersten Reihen eines Dataframes zurück
+?tail # Gibt die letzten Reihen eines Dataframes zurück
+?which.max # Gibt die Reihenposition des höchsten Wertes einer Variablen (in einem Dataframe) zurück
+?which.min # Gibt die Reihenposition des niedrigsten Wertes einer Variablen (in einem Dataframe) zurück
+?max # Gibt den höchsten Wert einer Variable (in einem Dataframe) zurück
+?min # Gibt den niedrigsten Wert einer Variable (in einem Dataframe) zurück
+
+# Missing Values 99
+# mydata$v1[mydata$v1 == 99] <- NA # mydata = dataframe, v1 = variable
+# x[!complete.cases(x),] # Gibt die Zeilen mit Missing values in einem Dataframe zur�ck
 
 # Deskriptive Statistik
 ?mean # Arithmetische Mitteln einer Variablen
@@ -29,22 +57,6 @@ chooseCRANmirror() # Wählt den Server aus, von dem die Packete installiert werd
 
 ?boxplot
 ?hist
-
-# Dataframes
-?str # Gibt die Variablentypen im Dataframe zurück
-?names # Gibt die Namen der Variablen zurück
-?subset # Verkleinerung eines Dataframes mit verschiedenen Bedingungen
-?nrow # Anzahl der Reihen eines Dataframes
-?ncol # Anazl der Spalten eines Dataframes
-
-?head # Gibt die ersten Reihen eines Dataframes zurück
-?tail # Gibt die letzten Reihen eines Dataframes zurück
-
-?which.max # Gibt die Reihenposition des höchsten Wertes einer Variablen (in einem Dataframe) zurück
-?which.min # Gibt die Reihenposition des niedrigsten Wertes einer Variablen (in einem Dataframe) zurück
-
-?max # Gibt den höchsten Wert einer Variable (in einem Dataframe) zurück
-?min # Gibt den niedrigsten Wert einer Variable (in einem Dataframe) zurück
 
 # Psych-Packet
 library(psych)
